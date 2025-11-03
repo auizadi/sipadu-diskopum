4ab80a192813d0d53c3be03822d8ee59d33e21be
auizadi <auizaldi.21026@mhs.unesa.ac.id>
2025-11-03 08:13:26 +0700

    fix: :bug: try fixes sime issues

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
-import 'flowbite';
+import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
+import "flowbite";
 
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+Alpine.plugin(persist);
